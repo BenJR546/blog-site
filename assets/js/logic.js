7413a300c16dcb3703c2b7810114671b2bdd3546
BenJR546 <162855613+BenJR546@users.noreply.github.com>
2024-07-01 13:37:22 +1000

    Added dark mode toggle functionality and fixed styling

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,8 @@
 // TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
+function toggleDarkMode(){
+    var element = document.body;
+    element.classList.toggle("dark");
+}
 
+document.getElementById("toggleDarkLight").addEventListener("click", toggleDarkMode);
 // TODO: Create functions to read and write from local storage
